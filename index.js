3a8e0bc11018b4c58c1d7b5390d83021596b91a1
Cyrus Li <70495046+Sothis-baka@users.noreply.github.com>
2021-06-11 12:11:31 -0500

    handle file receive

--- index.js
+++ index.js
@@ -3,23 +3,42 @@ const SpeechToTextV1 = require('ibm-watson/speech-to-text/v1');
 const { IamAuthenticator } = require('ibm-watson/auth');
 
 const express = require('express');
+const fileUpload = require('express-fileupload');
+
 const app = express();
 const port = process.env.PORT || 80;
 
-app.get('/', (req, res) => {
+app.use(fileUpload());
+
+app.get('/api', (req, res) => {
     res.send('Hello World!')
 });
 
-app.post('/', (req, res) => {
-    console.log('received a post, body', req.body)
-    res.send('Hello World!')
+app.post('/api/uploadFile', (req, res) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    let sampleFile = req.files.file;
+
+    // Use the mv() method to place the file somewhere on your server
+    sampleFile.mv( __dirname + '/audios/' + 'temp.mp3', async (err) => {
+        if (err)
+            return res.status(500).send(err);
+
+        const response = await recognize();
+
+        res.status(200).send(response);
+    });
 });
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 });
 
-const recognize = () => {
+const recognize = async () => {
     const speechToText = new SpeechToTextV1({
         authenticator: new IamAuthenticator({
             apikey: '37C9T7XWUjYDiG7YyLSsxu_YXtakx6sSjI5JBHXs_ckF',
@@ -28,18 +47,11 @@ const recognize = () => {
     });
 
     const recognizeParams = {
-        audio: fs.createReadStream('audio-file.flac'),
-        contentType: 'audio/flac',
-        wordAlternativesThreshold: 0.9,
-        keywords: ['colorado', 'tornado', 'tornadoes'],
-        keywordsThreshold: 0.5,
+        audio: fs.createReadStream('audios/temp.mp3'),
+        contentType: 'audio/mp3',
+        wordAlternativesThreshold: 0.9
     };
 
-    speechToText.recognize(recognizeParams)
-        .then(speechRecognitionResults => {
-            console.log(JSON.stringify(speechRecognitionResults, null, 2));
-        })
-        .catch(err => {
-            console.log('error:', err);
-        });
+    const result = await speechToText.recognize(recognizeParams);
+    return result.result;
 }
